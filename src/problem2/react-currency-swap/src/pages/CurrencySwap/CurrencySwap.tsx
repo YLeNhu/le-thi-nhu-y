ab0d2fbcf19df419c7699b5ee021822849c4bc3b
Y Le Nhu <imyour2603@gmail.com>
2024-06-15 14:07:44 +0700

    refactor component structure

--- src/problem2/react-currency-swap/src/pages/CurrencySwap/CurrencySwap.tsx
+++ src/problem2/react-currency-swap/src/pages/CurrencySwap/CurrencySwap.tsx
@@ -2,8 +2,18 @@ import CurrencySwapForm from './components/CurrencySwapForm'
 
 export default function CurrencySwap() {
   return (
-    <div>
+    <div className='welcome w-screen h-screen bg-white relative'>
+      {/* Currency swap form section */}
       <CurrencySwapForm />
+
+      {/* Background section */}
+      <div className='h-1/2 w-full bg-gradient-to-l from-blue-800 to-indigo-900 text-center p-4 lg:p-10 shadow-lg absolute top-0 left-0'>
+        <p className='text-white text-4xl font-bold'>Quick Assets Swap</p>
+        <p className='text-white text-xl p-2'>Swap assets with live exchange rate</p>
+      </div>
+
+      {/* Credit section */}
+      <p className='absolute bottom-0 w-full text-center text-sm p-2'>y.contact.here@gmail.com</p>
     </div>
   )
 }
