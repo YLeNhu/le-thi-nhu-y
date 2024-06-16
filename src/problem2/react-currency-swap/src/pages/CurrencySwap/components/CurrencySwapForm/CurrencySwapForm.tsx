e125c83e0fe12e0da79dcf210ae1576def6c8ffd
Y Le Nhu <imyour2603@gmail.com>
2024-06-16 14:37:29 +0700

    feat: add handleSubmit to swap currency

--- src/problem2/react-currency-swap/src/pages/CurrencySwap/components/CurrencySwapForm/CurrencySwapForm.tsx
+++ src/problem2/react-currency-swap/src/pages/CurrencySwap/components/CurrencySwapForm/CurrencySwapForm.tsx
@@ -1,5 +1,5 @@
-import { InputNumber, InputNumberProps, Select } from 'antd'
-import { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { InputNumber, Select, Button } from 'antd'
 import { useSelector } from 'react-redux'
 import { RootState } from 'store'
 import { Currency } from 'types/currency.type'
@@ -11,50 +11,50 @@ import { labelClasses } from 'constants/common-classes'
 const CurrencySwapForm = () => {
   const currencyList: Currency[] = useSelector((state: RootState) => state.currency.currencyList)
   const loading: boolean = useSelector((state: RootState) => state.currency.loading)
+  const currencyOptions = sanitizeCurrencyData(currencyList)
 
-  const currencyOptions = sanitizeCurrencyData(currencyList);
+  const [inputAmount, setInputAmount] = useState<number | null>(1)
+  const [outputAmount, setOutputAmount] = useState<number | null>(1)
+  const [inputCurrency, setInputCurrency] = useState('ETH')
+  const [outputCurrency, setOutputCurrency] = useState('USDC')
 
-  const [inputAmount, setInputAmount] = useState<number | null>(1);
-  const [outputAmount, setOutputAmount] = useState<number | null>(1);
+  const calculateBasedOnRate = useCallback((data: CurrencyFormData, reverse: boolean = false) => {
+    const inputCurrencyData = currencyList.find((item) => item.currency === data.inputCurrency)
+    const outputCurrencyData = currencyList.find((item) => item.currency === data.outputCurrency)
 
-  const [inputCurrency, setInputCurrency] = useState('ETH');
-  const [outputCurrency, setOutputCurrency] = useState('USDC');
+    if (inputCurrencyData && outputCurrencyData) {
+      const rate = outputCurrencyData.price !== 0 ? inputCurrencyData.price / outputCurrencyData.price : 0
+      if (reverse) {
+        const convertedInputAmount = data.outputAmount ? parseFloat((data.outputAmount / rate).toPrecision(7)) : 0
+        setInputAmount(convertedInputAmount)
+      } else {
+        const convertedOutputAmount = data.inputAmount ? parseFloat((rate * data.inputAmount).toPrecision(7)) : 0
+        setOutputAmount(convertedOutputAmount)
+      }
+    }
+  }, [currencyList])
 
-  const calculateBasedOnRate = (data: CurrencyFormData) => {
-
-    const inputPrice = currencyList.filter((item) => item.currency == data.inputCurrency)[0].price;
-    const outputPrice = currencyList.filter((item) => item.currency == data.outputCurrency)[0].price;
-    const rate = outputPrice !== 0 ? inputPrice / outputPrice : 0;
-
-    const convertedOutputAmount = data.inputAmount ? parseFloat((rate * data.inputAmount).toFixed(5)) : 0;
-    setOutputAmount(convertedOutputAmount);
-  }
+  useEffect(() => {
+    calculateBasedOnRate({ inputAmount, outputAmount, inputCurrency, outputCurrency }, false)
+  }, [inputAmount, inputCurrency, outputCurrency, calculateBasedOnRate])
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const formData: CurrencyFormData = {
-      inputAmount: inputAmount,
-      outputAmount: outputAmount,
-      inputCurrency: inputCurrency,
-      outputCurrency: outputCurrency,
-    }
-    console.log(formData);
-    calculateBasedOnRate(formData);
+    setInputCurrency(outputCurrency)
+    setOutputCurrency(inputCurrency)
+    setInputAmount(outputAmount)
+    setOutputAmount(inputAmount)
   }
 
-  const onInput: InputNumberProps['onInput'] = (value) => {
-    const inputValue = parseFloat(value);
-    setInputAmount(inputValue);
+  const handleInputAmountChange = (value: number | null) => {
+    setInputAmount(value)
+    calculateBasedOnRate({ inputAmount: value, outputAmount, inputCurrency, outputCurrency }, false)
+  }
 
-    const formData = {
-      inputAmount: inputValue,
-      outputAmount: outputAmount,
-      inputCurrency: inputCurrency,
-      outputCurrency: outputCurrency,
-    };
-
-    calculateBasedOnRate(formData);
-  };
+  const handleOutputAmountChange = (value: number | null) => {
+    setOutputAmount(value)
+    calculateBasedOnRate({ inputAmount, outputAmount: value, inputCurrency, outputCurrency }, true)
+  }
 
   return (
     <>
@@ -65,95 +65,32 @@ const CurrencySwapForm = () => {
           onSubmit={handleSubmit}
           className='bg-white z-10 w-full max-w-5xl rounded-lg shadow-lg shadow-black/20 px-10 py-10 text-left absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
         >
-          {/* AMOUNT TO SEND SECTION */}
-          <div className='flex space-x-4 mb-4'>
-            <div className='w-2/3'>
-              <label className={labelClasses} htmlFor='input-amount'>
-                Amount to send
-              </label>
-
-              <InputNumber
-                id='input-amount'
-                size='large'
-                placeholder='Enter amount'
-                type='number'
-                min={0}
-                style={{ width: '100%' }}
-                value={inputAmount}
-                // onChange={(value) => handleChangeCurrency(value as number, 'inputAmount')}
-                onInput={onInput}
-              />
-            </div>
-
-            <div className='w-1/3 h-full'>
-              <label className={labelClasses} htmlFor='send-currency'>
-                Currency
-              </label>
-
-              <Select
-                id='send-currency'
-                options={currencyOptions}
-                style={{ width: '100%' }}
-                size='large'
-                value={inputCurrency}
-                showSearch
-                onChange={(value) => setInputCurrency(value)}
-                allowClear
-                autoClearSearchValue
-              />
-
-            </div>
-          </div>
-
-          {/* AMOUNT TO RECEIVE SECTION */}
-          <div className='flex space-x-4 mb-4'>
-            <div className='w-2/3'>
-              <label className={labelClasses} htmlFor='output-amount'>
-                Amount to receive
-              </label>
-
-              <InputNumber
-                id='output-amount'
-                size='large'
-                placeholder='Enter amount'
-                type='number'
-                min={0}
-                style={{ width: '100%' }}
-                value={outputAmount}
-                onChange={(value) => setOutputAmount(value as number)}
-              />
-            </div>
-
-            <div className='w-1/3 h-full'>
-              <label className={labelClasses} htmlFor='receive-currency'>
-                Currency
-              </label>
-
-              <Select
-                id='receive-currency'
-                style={{ width: '100%' }}
-                size='large'
-                value={outputCurrency}
-                showSearch
-                options={currencyOptions}
-                onChange={(value) => setOutputCurrency(value)}
-                allowClear
-                autoClearSearchValue
-              />
-            </div>
-          </div>
-
+          <AmountInputSection
+            label="Amount to send"
+            amount={inputAmount}
+            onAmountChange={handleInputAmountChange}
+            currency={inputCurrency}
+            onCurrencyChange={setInputCurrency}
+            currencyOptions={currencyOptions}
+          />
+          <AmountInputSection
+            label="Amount to receive"
+            amount={outputAmount}
+            onAmountChange={handleOutputAmountChange}
+            currency={outputCurrency}
+            onCurrencyChange={setOutputCurrency}
+            currencyOptions={currencyOptions}
+          />
           <div className='flex flex-col space-y-4'>
             <div className='flex items-center justify-between'>
               <div className='inline-flex items-center text-base font-semibold text-gray-900'>$2367</div>
-
-              {/* Swap button */}
-              <button
-                type='submit'
+              <Button
+                type='primary'
+                htmlType='submit'
                 className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
               >
                 CONFIRM SWAP
-              </button>
+              </Button>
             </div>
           </div>
         </form>
@@ -161,5 +98,49 @@ const CurrencySwapForm = () => {
     </>
   )
 }
+
+interface AmountInputSectionProps {
+  label: string
+  amount: number | null
+  onAmountChange: (value: number | null) => void
+  currency: string
+  onCurrencyChange: (value: string) => void
+  currencyOptions: { label: string, value: string }[]
+}
+
+const AmountInputSection: React.FC<AmountInputSectionProps> = ({ label, amount, onAmountChange, currency, onCurrencyChange, currencyOptions }) => (
+  <div className='flex space-x-4 mb-4'>
+    <div className='w-2/3'>
+      <label className={labelClasses} htmlFor={`${label.toLowerCase().replace(/ /g, '-')}`}>
+        {label}
+      </label>
+      <InputNumber
+        id={`${label.toLowerCase().replace(/ /g, '-')}`}
+        size='large'
+        placeholder='Enter amount'
+        min={0}
+        style={{ width: '100%' }}
+        value={amount}
+        onChange={onAmountChange}
+      />
+    </div>
+    <div className='w-1/3 h-full'>
+      <label className={labelClasses} htmlFor={`${label.toLowerCase().replace(/ /g, '-')}-currency`}>
+        Currency
+      </label>
+      <Select
+        id={`${label.toLowerCase().replace(/ /g, '-')}-currency`}
+        options={currencyOptions}
+        style={{ width: '100%' }}
+        size='large'
+        value={currency}
+        showSearch
+        onChange={onCurrencyChange}
+        allowClear
+        autoClearSearchValue
+      />
+    </div>
+  </div>
+)
 
 export default CurrencySwapForm
